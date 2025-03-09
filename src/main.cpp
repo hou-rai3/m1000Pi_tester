bc724bc11accb8404aaaa9d2673c21fc62f8f834
Okabe Minato <166805519+hou-rai3@users.noreply.github.com>
2025-03-09 15:31:54 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,23 @@
+#include "C810.hpp"
 #include "mbed.h"
 
 BufferedSerial pc(USBTX, USBRX, 115200);
 InterruptIn button(BUTTON1);
-PwmOut servo(D6);
+C810 c810(D6); // PWMピン指定
 auto pre = HighResClock::now();
-int target;
+int target = 1000;
 
-void c810_setup() { // 初回のみ実行。二回目以降は不要
-    printf("target start\n");
-    target = 2000;
-    servo.pulsewidth_us(target);
-    printf("wait 2000ms\n");
-    ThisThread::sleep_for(2000ms);
-    target = 1000;
-    servo.pulsewidth_us(target);
-    printf("target end\n");
-    ThisThread::sleep_for(2000ms);
-}
-
-void fire_c810() {
-    if (!button.read()) {
-        target = 1500;
-    } else {
-        target = 1000;
-    }
-    servo.pulsewidth_us(target);
-}
 int main() {
-    // c810_setup();
+    // c810.setup();
     button.mode(PullUp);
     while (1) {
-        fire_c810();
+        if (!button.read()) {
+            target = std::max(2000, target + 3);
+            c810.write(target);
+        } else {
+            target = std::max(1000, target - 3);
+            c810.write(target);
+        }
+        ThisThread::sleep_for(10ms);
     }
 }
